fc8d81168c6c79da4cbdc272a1830e05e29b160d
maestro <t.finn@meteowindow.com>
2016-12-19 15:51:56 +0100

    Added wrapper for xarray functions. Renamed xr_plot to data_plot.

--- pymepps/metdata/metdata.py
+++ pymepps/metdata/metdata.py
@@ -26,6 +26,8 @@ Created for pymepps
 import logging
 
 # External modules
+import pandas as pd
+import xarray as xr
 
 # Internal modules
 
@@ -42,10 +44,55 @@ class MetData(object):
         self.data_origin = data_origin
         self.data = data
 
+    def __getattr__(self, key):
+        if key == 'data':
+            logger.exception(" ".join([
+                "Can't access data attribute.",
+                "Did you try to access properties before",
+                "loading data?"
+            ]))
+        return self._xr_function(key)
+
+    def __getitem__(self, key):
+        return type(self)(self.data.__getitem__(key), self.data_origin)
+
+    def __setitem__(self, key, value):
+        return self.data.__setitem__(key, value)
+
     def __call__(self):
         return self.data
 
-    def xr_plot(self, **kwargs):
+    def _xr_function(self, key):
+        """
+        Get data function with given key. This is a wrapper around
+        type(self.data) functions to secure a proper return value.
+
+        Parameters
+        ----------
+        key : str
+            The function which should be called. Have to be an available
+            function for type of self.data!
+
+        Returns
+        -------
+        wrapped_func : function
+            The wrapped type(self.data) function. The wrapped function returns
+            a new TS/SpatialData instance, if the result of the function is a
+            type(self.data), else the return value of the function will be
+            returned.
+        """
+        def wrapped_func(*args, **kwargs):
+            try:
+                result = getattr(self.data, key)(*args, **kwargs)
+            except TypeError:
+                result = getattr(self.data, key)
+            if isinstance(result, (pd.DataFrame, pd.Series, xr.DataArray)):
+                return type(self)(result, self.data_origin)
+            else:
+                return result
+        return wrapped_func
+
+    def data_plot(self, **kwargs):
         """
         Method to refer to the xr_plot layer.
         Parameters
