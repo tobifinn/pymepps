6a1216befc1542512b9718c68c850a1990cb951c
maestro <t.finn@meteowindow.com>
2016-12-19 15:50:24 +0100

    Filled TimeSeriesDataset class. At the moment this class is tested only for Wettermast data, but should work for every possible TimeSeriesHandler.

--- pymepps/metdata/tsdataset.py
+++ pymepps/metdata/tsdataset.py
@@ -26,13 +26,76 @@ Created for pymepps
 import logging
 
 # External modules
+import pandas as pd
 
 # Internal modules
 from .metdataset import MetDataset
+from .tsdata import TSData
 
 
 logger = logging.getLogger(__name__)
 
 
 class TSDataset(MetDataset):
-    pass
+    def __init__(self, file_handlers, data_origin=None):
+        """
+        TSDataset is a class for a pool of file handlers. Typically a
+        time series dataset combines the files of a station, such that it
+        is possible to select a variable and get a TSData instance. For
+        memory reasons the data of a variable is only loaded if it is selected.
+
+        Parameters
+        ----------
+        file_handlers : list of childs of FileHandler
+            The spatial dataset is based on these files. The files should be
+            either instances of NetCDFHandler, TextHandler.
+        data_origin : optional
+            The data origin. This parameter is important to trace the data
+            flow. If this is None, there is no data origin and this
+            dataset will be the starting point of the data flow. Default is
+            None.
+
+        Methods
+        -------
+        select
+            Method to select a variable.
+        """
+        super().__init__(file_handlers, data_origin)
+
+    @property
+    def lon_lat(self):
+        return self._get_lon_lat()
+
+    def _get_lon_lat(self):
+        if self.data_origin is not None:
+            try:
+                return self.data_origin.lon_lat()
+            except Exception as e:
+                logger.debug('Couldn\'t get lon/lat from data origin, due to '
+                             '{0:s}'.format(str(e)))
+        else:
+            try:
+                return self.file_handlers[0].lon_lat()
+            except Exception as e:
+                logger.debug('Couldn\'t get lon/lat from first file handler, '
+                             'due to {0:s}'.format(str(e)))
+                return None
+
+    def _get_file_data(self, file, var_name):
+        return file.get_timeseries(var_name)
+
+    def data_merge(self, data):
+        extracted_data = data[0]
+        for d in data[1:]:
+            for k in d:
+                if k in extracted_data.keys():
+                    extracted_data[k] = extracted_data[k].append(d[k])
+                    extracted_data[k] = extracted_data[k].sort_index()
+                else:
+                    extracted_data.update({k:d[k]})
+        if len(extracted_data.keys())>1:
+            extracted_data = pd.DataFrame(extracted_data)
+        else:
+            extracted_data = pd.Series(list(extracted_data.values())[0])
+        return TSData(extracted_data, self, lonlat=self.lon_lat)
+
