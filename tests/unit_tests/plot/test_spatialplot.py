3cd3ef37c8d11c78e6e2bb02baefaae942ba8a13
Tobias Finn <t.finn@meteowindow.com>
2017-01-11 09:03:45 +0100

    Added SpatialPlot as ChildClass of BasePlot. SpatialPlot has only a changed subplot_type in comparison, so the unit test could be also inherit. Unit tests passed.

---
+++ tests/unit_tests/plot/test_spatialplot.py
@@ -0,0 +1,44 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on 10.01.17
+
+Created for pymepps
+
+@author: Tobias Sebastian Finn, tobias.sebastian.finn@studium.uni-hamburg.de
+
+    Copyright (C) {2017}  {Tobias Sebastian Finn}
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+# System modules
+import unittest
+
+# External modules
+
+# Internal modules
+from pymepps.plot.spatial.spatialplot import SpatialPlot
+from pymepps.plot.spatial.spatialsubplot import SpatialSubplot
+from .test_plot import TestPlot
+
+
+class TestSpatialPlot(TestPlot):
+    def setUp(self):
+        super().setUp()
+        self.plot_class = SpatialPlot
+        self.subplot_type = SpatialSubplot
+
+
+if __name__ == '__main__':
+    unittest.main()
