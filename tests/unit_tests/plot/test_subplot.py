4a14bcbdeb9be4c76bad8d9ad7a3650e86aed71d
Tobias Finn <t.finn@meteowindow.com>
2017-01-11 09:16:49 +0100

    Added compability for generic subplot types in subplot unit test. Unit tests passed.

--- tests/unit_tests/plot/test_subplot.py
+++ tests/unit_tests/plot/test_subplot.py
@@ -37,53 +37,56 @@ from pymepps.utilities.testcase import TestCase
 
 
 class TestSubplot(TestCase):
+    def setUp(self):
+        self.subplot_type = Subplot
+
     def tearDown(self):
         plt.close('all')
 
     def test_axes_is_plt_subplot(self):
-        sp = Subplot()
+        sp = self.subplot_type()
         self.assertIsInstance(sp.ax, type(plt.subplot()))
 
     def test_attributes_from_mpl_axes(self):
-        sp = Subplot()
+        sp = self.subplot_type()
         self.assertAttribute(sp, 'set_xlim')
 
     def test_gridspec_as_parameter(self):
-        sp = Subplot()
+        sp = self.subplot_type()
         gs = matplotlib.gridspec.GridSpec(4,4)
-        sp_gs = Subplot(None, gs[3])
+        sp_gs = self.subplot_type(None, gs[3])
         self.assertNotEqual(sp.get_subplotspec().get_geometry(),
                             sp_gs.get_subplotspec().get_geometry())
 
     def test_plot_method_return_self(self):
-        sp = Subplot()
+        sp = self.subplot_type()
         after_sp = sp.plot_method(data=(range(10), range(10)))
         self.assertEqual(sp, after_sp)
 
     def test_plot_method_not_empty(self):
-        sp = Subplot()
+        sp = self.subplot_type()
         self.assertGreaterEqual(len(sp._plot_methods), 1)
 
     def test_plot_method(self):
-        sp = Subplot()
+        sp = self.subplot_type()
         sp.plot(data=(range(10), range(10)))
         plot_data = sp.ax.lines[0].get_data()
-        sp = Subplot()
+        sp = self.subplot_type()
         sp.plot_method(data=(range(10), range(10)), method='plot')
         method_data = sp.ax.lines[0].get_data()
         numpy.testing.assert_array_equal(plot_data, method_data)
 
     def test_not_available_plot_method_raises(self):
-        sp = Subplot()
+        sp = self.subplot_type()
         with self.assertRaises(ValueError):
             sp.plot_method(data=(range(10), range(10)), method='plot12')
 
     def test_not_available_stylesheet_raises_error(self):
         with self.assertRaises(ValueError):
-            sp = Subplot(stylesheets=['fivethirtyeight32'])
+            self.subplot_type(stylesheets=['fivethirtyeight32'])
 
     def test_stylesheets_none_basis_stylesheet(self):
-        sp = Subplot()
+        sp = self.subplot_type()
         numpy.testing.assert_array_equal(sp.stylesheets, ['default'])
 
 if __name__ == '__main__':
