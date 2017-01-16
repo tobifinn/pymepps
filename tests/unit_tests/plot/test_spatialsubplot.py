11fd290f9285aa20a7a19f8ac87745bfce1f5d1d
Tobias Finn <t.finn@meteowindow.com>
2017-01-16 17:44:42 +0100

    -

--- tests/unit_tests/plot/test_spatialsubplot.py
+++ tests/unit_tests/plot/test_spatialsubplot.py
@@ -72,16 +72,24 @@ class TestSpatialSubplotPlot(TestSubplot):
         with self.assertRaises(ValueError):
             sp._extract_data(self.subplot_type())
 
-    def test_extract_data_extracts_dataarray(self):
+    def test_extract_data_dataarray(self):
         file = os.path.join(BASE_DIR, 'test_data', 'spatial',
                             'test_spatialdata_dataarray.pk')
+        print(file)
+        self.fail(file)
         test_data = pickle.load(open(file, mode='rb'))
-        sp = self.subplot_type()
-        x, y, plot_data = sp._extract_data(test_data)
-        numpy.testing.assert_array_equal(test_data['lat'], x)
-        numpy.testing.assert_array_equal(test_data['lon'], y)
-        numpy.testing.assert_array_equal(test_data.values.squeeze(), plot_data)
+        # sp = self.subplot_type()
+        # x, y, plot_data = sp._extract_data(test_data)
+        # numpy.testing.assert_array_equal(test_data['lat'], x)
+        # numpy.testing.assert_array_equal(test_data['lon'], y)
+        # numpy.testing.assert_array_equal(test_data.values.squeeze(), plot_data)
 
+    def test_extract_data_extracts_dataarray(self):
+        sp = self.subplot_type()
+        x, y, plot_data = sp._extract_data(self.test_data[2])
+        numpy.testing.assert_array_equal(self.test_data[0], x)
+        numpy.testing.assert_array_equal(self.test_data[1], y)
+        numpy.testing.assert_array_equal(self.test_data[2], plot_data)
 
 
 if __name__ == '__main__':
