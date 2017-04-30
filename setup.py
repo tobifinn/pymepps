d8868fa78bdc32051718d27c4060859aadb9b520
Tobias Finn <t.finn@meteowindow.com>
2017-04-30 12:13:33 +0200

    Updated version numbering of setup.py

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ from os import path
 # External modules
 
 # Internal modules
+import pymepps
 
 __version__ = "0.1"
 
@@ -40,7 +41,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1',
+    version=pymepps.__version__,
 
     description='A meteorological post-processing system based on python',
 
@@ -54,29 +55,9 @@ setup(
     # Choose your license
     license='GPL3',
 
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-#     classifiers=[
-#         # How mature is this project? Common values are
-#         #   3 - Alpha
-#         #   4 - Beta
-#         #   5 - Production/Stable
-#         'Development Status :: 3 - Alpha',
-#
-#         # Pick your license as you wish (should match "license" above)
-#         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-#
-#         # Specify the Python versions you support here. In particular, ensure
-#         # that you indicate whether you support Python 2, Python 3 or both.
-#         'Programming Language :: Python :: 3.5',
-#
-#         'Topic :: Scientific / Engineering',
-#         'Topic :: Scientific/Engineering :: Artificial Intelligence',
-#         'Topic :: Scientific/Engineering :: Information Analysis',
-#         'Topic :: Software Development :: Libraries :: Python Modules',
-# ],
-
     # What does your project relate to?
-    keywords='statistics meteorology post-processing system forecast verification plotting',
+    keywords='statistics meteorology post-processing system forecast '
+             'verification plotting',
     packages=["pymepps"],
     package_dir={"pymepps": "pymepps"},
 )
