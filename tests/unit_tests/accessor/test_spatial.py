9bd865e56a6f73701b22d0a1397ec295621dec38
Tobias Finn <t.finn@meteowindow.com>
2017-07-06 10:22:04 +0200

    Added .gitattributes

--- tests/unit_tests/accessor/test_spatial.py
+++ tests/unit_tests/accessor/test_spatial.py
@@ -1,7 +1,7 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 #
-#Created on 01.05.17
+#Created on 06.07.17
 #
 #Created for pymepps
 #
@@ -28,12 +28,8 @@ import unittest
 import logging
 
 # External modules
-import xarray as xr
-import numpy as np
 
 # Internal modules
-from pymepps.metdata import SpatialDataset
-from pymepps.metfile import NetCDFHandler
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -41,21 +37,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 logging.basicConfig(level=logging.DEBUG)
 
 
-class TestSpatialDS(unittest.TestCase):
-    def setUp(self):
-        self.file = '/home/tfinn/Data/master_thesis/test_data/nc/20160607_' \
-                    '0000/det/lfff00000000.nc'
-        self.handler = NetCDFHandler(self.file)
-        self.ds = SpatialDataset([self.handler,])
-        self.xr_ds = xr.open_dataset(self.file)
-
-    def tearDown(self):
-        self.handler.close()
-
-    def test_data_merge(self):
-        self.handler.open()
-        messages = self.handler.get_messages('T')
-        self.ds.data_merge(messages, 'T')
+class TestClass(unittest.TestCase):
+    pass
 
 
 if __name__ == '__main__':
