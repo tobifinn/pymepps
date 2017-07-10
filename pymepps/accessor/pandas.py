9449e3714403b89378b1125c9656678960162dc9
Tobias Finn <t.finn@meteowindow.com>
2017-07-10 15:44:23 +0200

    Removed plot and tests/old to slim down the repository.

--- pymepps/accessor/pandas.py
+++ pymepps/accessor/pandas.py
@@ -43,11 +43,13 @@ logger = logging.getLogger(__name__)
 @register_dataframe_accessor('pp')
 class PandasAccessor(MetData):
     """
-    An accessor as extension to pandas data structures. This accessor is a base
-    for a SeriesAccessor and FrameAccessor.
+    An accessor to extend the pandas data structure. This could be used more
+    actively in the  future to add more post-processing specific features to
+    pandas.
     """
     def __init__(self, data):
         super().__init__(data)
+        self.lonlat = None
 
     def update(self, *items):
         update_data = [self.data.copy(), ]
@@ -70,3 +72,70 @@ class PandasAccessor(MetData):
             updated_array.loc[ind] = updated_array.loc[ind].fillna(val)
         updated_array.pp.lonlat = self.lonlat
         return updated_array
+
+    def save(self, save_path):
+        """
+        The data is saved as json file. The pandas to_json method is used to
+        generate convert the data to json. If lonlat was given it will be saved
+        under a lonlat key. Json is used instead of HDF5 due to possible
+        corruption problems.
+
+        Parameters
+        ----------
+        save_path: str
+            Path where the json file should be saved.
+        """
+        save_dict = dict(data=self.data.to_json(orient='split'),
+                         lonlat=self.lonlat)
+        with open(save_path, mode='w+') as fp:
+            json.dump(save_dict, fp)
+
+    @staticmethod
+    def load(load_path):
+        """
+        Load the given json file and return a TSData instance with the loaded
+        file. The loader uses tries to locate the lonlat and the data keys
+        within the json file. If there are not these keys the loader tries to
+        load the whole json file into pandas.
+
+        Parameters
+        ----------
+        load_path: str
+            Path to the json file which should be loaded. It is recommended to
+            load only previously saved TSData instances.
+
+        Returns
+        -------
+        load_data: pandas object
+            The loaded pandas object.
+        """
+        if isinstance(load_path, str):
+            fp = open(load_path, mode='r')
+        elif getattr(load_path, 'read'):
+            fp = load_path
+        else:
+            raise TypeError('Path needs to be either a string '
+                            'or an opened file!')
+        json_str = fp.read()
+        if not isinstance(json_str, str):
+            json_str = json_str.decode()
+        saved_json_instance = json.loads(json_str)
+        fp.close()
+        if 'lonlat' in list(saved_json_instance.keys()) and \
+                saved_json_instance['lonlat'] is not None:
+            lonlat = tuple(saved_json_instance['lonlat'])
+        else:
+            lonlat = None
+        if 'data' in list(saved_json_instance.keys()):
+            pd_data_json = saved_json_instance['pd_data']
+
+        else:
+            pd_data_json = saved_json_instance
+        try:
+            load_data = pd.read_json(pd_data_json, orient='split',
+                                     typ='frame')
+        except ValueError:
+            load_data = pd.read_json(pd_data_json, orient='split',
+                                     typ='series')
+        load_data.pp.lonlat = lonlat
+        return load_data
